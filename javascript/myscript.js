d361330997ea4311692729996cac426ee449af46
ammeum <98023937+ammeum@users.noreply.github.com>
2022-11-30 10:40:35 +0100

    Add files via upload

---
+++ javascript/myscript.js
@@ -0,0 +1,13 @@
+function mobilMenu(){
+
+  var x = document.getElementById("ham");
+
+  if(x.style.display === "block") {
+      x.style.display = "none";
+  }
+   else {
+      x.style.display = "block";
+  }
+}
+
+// Koden er hentet fra min egen innlevering fra Webutvikling 2021
